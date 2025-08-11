7ca9199da6482597c02951bfce54ed7f4ddde7a6
crueter <crueter@eden-emu.dev>
2025-08-11 16:49:46 -0400

    oops
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- pack.sh
+++ pack.sh
@@ -2,9 +2,9 @@
 
 mkdir -p artifacts
 
-ARCHES="amd64 aarch64 steamdeck"
+ARCHES="amd64 steamdeck"
 if [ "$DEVEL" = "false" ]; then
-  ARCHES="$ARCHES legacy rog-ally armv9"
+  ARCHES="$ARCHES legacy rog-ally aarch64 armv9"
 fi
 
 for arch in $ARCHES
