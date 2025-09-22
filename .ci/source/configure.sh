b862b5d482985ba8b7126137ade6fa47b6e7e863
crueter <crueter@eden-emu.dev>
2025-09-22 15:40:08 -0400

    fix src pack
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- .ci/source/configure.sh
+++ .ci/source/configure.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+cd eden
+
 chmod a+x tools/cpm-fetch*.sh
 tools/cpm-fetch-all.sh
--- .ci/source/pack.sh
+++ .ci/source/pack.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-export ARCHIVE="git-archive-all-1.23.1"
-wget https://github.com/Kentzo/git-archive-all/releases/download/1.23.1/$ARCHIVE.tar.gz -nv
-tar xf $ARCHIVE.tar.gz
+cd eden
 
-python $ARCHIVE/git_archive_all.py --include .cache --include GIT-RELEASE --include GIT-COMMIT --include GIT-TAG --include GIT-REFSPEC --force-submodules source.tar
-
-zstd -10 source.tar
-rm source.tar
+tar --zstd -cf ../source.tar.zst * .*
